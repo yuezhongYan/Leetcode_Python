0ff99a9784af382f4dc88e1091aa1a0ccaefc924
yuezhongYan <65264957+yuezhongYan@users.noreply.github.com>
2020-05-22 21:02:28 -0400

    Add code for Problem 0371

---
+++ 0371_sum_of_two_integers_v1/Solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """Calculate the sum of two integers a and b without "+" or "-" sign
+        A hazy trick without "+" or "-" sign
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return sum([a, b])
---
+++ 0371_sum_of_two_integers_v1/__init__.py
@@ -0,0 +1,19 @@
+from Solution import Solution
+
+
+def main():
+    """runs solution method
+
+    """
+    solution = Solution()
+    a = 1
+    b = 2
+    print(solution.getSum(a, b))
+
+
+class SolutionTest(object):
+    """Test solution
+
+    """
+    if __name__ == '__main__':
+        main()
